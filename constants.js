fc0dffd87fd2932f57c640740a6ec8f6f63bb557
Douglas Naphas <douglasnaphas@gmail.com>
2019-03-07 10:20:35 -0500

    Handle multiple line types
    madliberation-130

--- constants.js
+++ constants.js
@@ -2,6 +2,7 @@ const constants = {
   PAGE_MARKER: /^\s*#\s*{{\s*Page\s*}}\s*$/i,
   LINE_MARKER: /[^\s]/,
   H1_MARKER: /^\s*#\s+(?=[^s])/,
-  H1_TYPE: 'h1'
+  H1_TYPE: 'h1',
+  DEFAULT_LINE_TYPE: 'p'
 };
 module.exports = constants;
