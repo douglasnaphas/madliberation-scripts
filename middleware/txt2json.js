a7e03da6230af19ed01d1b9c330456f5bee2642b
Douglas Naphas <douglasnaphas@gmail.com>
2020-01-04 22:17:58 -0500

    Send descriptive error message on bad request

--- middleware/txt2json.js
+++ middleware/txt2json.js
@@ -1,11 +1,19 @@
 const script2json = require('../lib/script2json');
 const txt2json = [
   (req, res, next) => {
-    console.log(
-      req.body &&
-        req.body.text &&
-        req.body.text == '# {{Page}}\n\n# The simplest possible script'
-    );
+    if (!(req.body && req.body.text)) {
+      return res.status(400).send({
+        error:
+          'POST body should be application/json,' +
+          ' should have a text property, and should start with a # {{Page}}\n marker',
+        exampleOKBody: {
+          text: '# {{Page}}\n\n# The simplest possible script'
+        }
+      });
+    }
+    return next();
+  },
+  (req, res, next) => {
     return res.send(script2json.parse(req.body.text));
   }
 ];
