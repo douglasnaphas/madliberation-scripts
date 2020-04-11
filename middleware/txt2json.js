a376287f9f04d17dd05cec9a80fe6e650c3c6010
Douglas Naphas <douglasnaphas@gmail.com>
2020-04-11 00:47:09 -0400

    Log from txt2json
    https://github.com/douglasnaphas/madliberation/issues/246

--- middleware/txt2json.js
+++ middleware/txt2json.js
@@ -1,20 +1,31 @@
-const script2json = require('../lib/script2json');
+const script2json = require("../lib/script2json");
 const txt2json = [
   (req, res, next) => {
     if (!(req.body && req.body.text)) {
       return res.status(400).send({
         error:
-          'POST body should be application/json,' +
-          ' should have a text property, and should start with a # {{Page}}\n marker',
+          "POST body should be application/json," +
+          " should have a text property, and should start with a # {{Page}}\n marker",
         exampleOKBody: {
-          text: '# {{Page}}\n\n# The simplest possible script'
-        }
+          text: "# {{Page}}\n\n# The simplest possible script",
+        },
       });
     }
     return next();
   },
   (req, res, next) => {
-    return res.send(script2json.parse(req.body.text));
-  }
+    console.log("middleware/txt2json: logging...");
+    let data;
+    try {
+      data = script2json.parse(req.body.text);
+      return res.send(data);
+    } catch (err) {
+      console.log(
+        "middleware/txt2json: error on data = script2json.parse(req.body.text)"
+      );
+      console.log(err);
+    }
+    return res.status(400).send("txt2json_error");
+  },
 ];
 module.exports = txt2json;
