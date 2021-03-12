ffdc6b1e09804ac442c655385b814bd1e01cf11c
Douglas Naphas <douglasnaphas@gmail.com>
2021-03-11 23:52:35 -0500

    Correct var names in bin/content2dynamo
    https://github.com/douglasnaphas/madliberation/issues/278

--- bin/content2dynamo.js
+++ bin/content2dynamo.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
 const program = require("commander");
-const script2json = require("../lib/script2json");
+const content2json = require("../lib/content2dynamo");
 
 program
-  .command("./txt2json.js")
+  .command("./content2dynamo.js")
   .version("1.0.0")
   .usage("<file>")
   .parse(process.argv);
@@ -13,4 +13,4 @@ if (process.argv.length < 3) {
   process.exit(1);
 }
 const f = process.argv[2];
-console.log(script2json.parseFile(f));
+console.log(content2json.parseFile(f));
