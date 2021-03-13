cfc5c159e12f8778c01844330a8b61aec52bfd74
Douglas Naphas <douglasnaphas@gmail.com>
2021-03-13 01:07:02 -0500

    Make bin/content2dynamo async
    https://github.com/douglasnaphas/madliberation/issues/278
    
    It's still not failing on db save errors.

--- bin/content2dynamo.js
+++ bin/content2dynamo.js
@@ -1,37 +1,43 @@
 #!/usr/bin/env node
 const program = require("commander");
 const content2dynamo = require("../lib/content2dynamo");
-program
-  .name("./content2dynamo.js")
-  .version("1.0.0")
-  .description(
-    "Put script metadata transformed from FILE, about scripts " +
-      "stored in BUCKET, in TABLE."
-  )
-  .requiredOption(
-    "-t, --table-name <TABLE>, The name of the DynamoDB table " +
-      "to put items in, example: -t MyTable123"
-  )
-  .requiredOption(
-    "-b, --bucket-name <BUCKET>, The name of the S3 bucket " +
-      "where script JSON is stored, example: -s MyBucket345"
-  )
-  .requiredOption(
-    "-c, --content-file-path <FILE>, The path to a file like " +
-      "content.json in this repo with data about scripts to be saved," +
-      " example: -c './my-content.json'"
-  )
-  .parse(process.argv);
-const opts = program.opts();
-const { tableName, bucketName, contentFilePath } = program.opts();
-console.log(tableName);
-console.log(bucketName);
-console.log(contentFilePath);
-const dbResponse = content2dynamo(tableName, bucketName, contentFilePath);
-console.log("bin/content2dynamo: dbResponse:");
-console.log(dbResponse);
-if (dbResponse.madliberation_error) {
-  console.log("bin/content2dynamo: batchWriting metadata failed, err:");
+(async () => {
+  program
+    .name("./content2dynamo.js")
+    .version("1.0.0")
+    .description(
+      "Put script metadata transformed from FILE, about scripts " +
+        "stored in BUCKET, in TABLE."
+    )
+    .requiredOption(
+      "-t, --table-name <TABLE>, The name of the DynamoDB table " +
+        "to put items in, example: -t MyTable123"
+    )
+    .requiredOption(
+      "-b, --bucket-name <BUCKET>, The name of the S3 bucket " +
+        "where script JSON is stored, example: -s MyBucket345"
+    )
+    .requiredOption(
+      "-c, --content-file-path <FILE>, The path to a file like " +
+        "content.json in this repo with data about scripts to be saved," +
+        " example: -c './my-content.json'"
+    )
+    .parse(process.argv);
+  const opts = program.opts();
+  const { tableName, bucketName, contentFilePath } = program.opts();
+  console.log(tableName);
+  console.log(bucketName);
+  console.log(contentFilePath);
+  const dbResponse = await content2dynamo(
+    tableName,
+    bucketName,
+    contentFilePath
+  );
+  console.log("bin/content2dynamo: dbResponse:");
   console.log(dbResponse);
-  process.exit(1);
-}
+  if (dbResponse.madliberation_error) {
+    console.log("bin/content2dynamo: batchWriting metadata failed, err:");
+    console.log(dbResponse);
+    process.exit(1);
+  }
+})();
